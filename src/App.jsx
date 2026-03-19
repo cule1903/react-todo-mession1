353c601cd53e49516628dafc7152d5b7aab61306
cule1903 <cule1903@gmail.com>
2026-03-19 16:01:36 +0900

    1단계: Vite + React 초기화

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>React Todo List</h1>
+      <p>2단계에서 개발될 예정입니다.</p>
+    </div>
+  )
+}
+
+export default App
