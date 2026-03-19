ae2f645c72d540a065d3aec39df73915e0da10e2
cule1903 <cule1903@gmail.com>
2026-03-19 16:25:19 +0900

    useEffect로 localStorage 자동 동기화

--- src/hooks/useTodos.js
+++ src/hooks/useTodos.js
@@ -4,35 +4,34 @@ import { loadTodos, saveTodos, createTodo } from '../utils/storage'
 export const useTodos = () => {
   const [todos, setTodos] = useState([])
 
-  // 초기 로드 - 마운트될 때 localStorage에서 로드
+  // 앱 최초 로딩 시 localStorage에서 todos 불러오기
   useEffect(() => {
     const loadedTodos = loadTodos()
     setTodos(loadedTodos)
   }, [])
 
+  // todos 배열이 변경될 때마다 localStorage에 자동 저장
+  useEffect(() => {
+    saveTodos(todos)
+  }, [todos])
+
   // 할 일 추가
   const addTodo = (value) => {
     if (!value.trim()) return
     const newTodo = createTodo(value)
-    const updatedTodos = [...todos, newTodo]
-    setTodos(updatedTodos)
-    saveTodos(updatedTodos)
+    setTodos([...todos, newTodo])
   }
 
   // 할 일 삭제
   const removeTodo = (id) => {
-    const updatedTodos = todos.filter(todo => todo.id !== id)
-    setTodos(updatedTodos)
-    saveTodos(updatedTodos)
+    setTodos(todos.filter(todo => todo.id !== id))
   }
 
   // 할 일 완료 상태 토글
   const toggleTodo = (id) => {
-    const updatedTodos = todos.map(todo =>
+    setTodos(todos.map(todo =>
       todo.id === id ? { ...todo, completed: !todo.completed } : todo
-    )
-    setTodos(updatedTodos)
-    saveTodos(updatedTodos)
+    ))
   }
 
   return {
